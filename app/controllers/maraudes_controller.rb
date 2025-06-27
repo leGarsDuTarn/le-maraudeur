180281eb7b16a4d94c52fb6bb7f6a35982ae0719
Benjamin Grassiano <grassiano.b@gmail.com>
2025-06-27 15:40:56 +0200

    corection syntaxe dependent, codage du controller participation et maraude

---
+++ app/controllers/maraudes_controller.rb
@@ -0,0 +1,54 @@
+class MaraudesController < ApplicationController
+  before_action :set_maraude, only: [show, edit, update, destroy]
+  def index
+    @maraudes = Maraude.all
+  end
+
+  def show
+    @maraude
+  end
+
+  def new
+    @maraude = Maraude.new
+  end
+
+  def edit
+    @maraude
+  end
+
+  def create
+    @maraude = Maraude.new(maraude_params)
+    @maraude.user = current_user
+      if @maraude.save
+        redirect_to @maraude, notice: 'La maraude a été créée avec succès.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def update
+    if @maraude.update(maraude_params)
+      redirect_to @maraude, notice: 'La maraude a été modifié avec succès.', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @maraude.destroy
+      redirect_to maraudes_url, notice: 'La maraude a été supprimé avec succès.'
+    else
+      redirect_to @maraude, alert: "La maraude n'a pas pu être supprimée."
+    end
+  end
+
+  private
+
+  def set_maraude
+    @maraude = Maraude.find(params[:id])
+  end
+
+  def maraude_params
+    params.require(:maraude).permit(:place, :date, :hour, :action)
+  end
+end
--- app/models/maraude.rb
+++ app/models/maraude.rb
@@ -1,6 +1,6 @@
 class Maraude < ApplicationRecord
   belongs_to :user
-  has_many :participations, dependant: :destroy
+  has_many :participations, dependent: :destroy
   has_many :users, through: :participations
 
   validates :place, :date, :hour, :action, presence: true
